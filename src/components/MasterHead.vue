680d55d385f00b02f90a377a31e90471d810f423
gem763 <16988092+gem763@users.noreply.github.com>
2022-08-30 01:55:25 +0900

    Update MasterHead.vue

--- src/components/MasterHead.vue
+++ src/components/MasterHead.vue
@@ -1,21 +1,43 @@
 <template>
-  <div class="header vcomp">
+  <div class="header vcomp" ref="header">
     <div class="logo">
       <div class="bg"></div>
       <img class='logopix' src="../assets/logo.png">
       <div class="title">차트킹</div>
       <div class="version">beta</div>
     </div>
+
+    <div class="topheader" :class="{on: topheader_on}">
+      <div class="logo">
+        <div class="title">차트킹</div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'MasterHead',
-  props: {
-    // msg: String
+<script setup>
+import { ref, onMounted } from 'vue';
+const header = ref(null);
+const hgap = ref(0);
+const topheader_on = ref(false);
+
+onMounted(() => {
+  document.querySelector('#app').addEventListener('scroll', recal_hgap);
+})
+
+const recal_hgap = () => {
+  // let target = e.target;
+  let rect = header.value.getBoundingClientRect();
+  hgap.value = rect.top + rect.height;
+
+  if (hgap.value < 0) {
+    topheader_on.value = true;
+
+  } else {
+    topheader_on.value = false;
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -85,4 +107,47 @@ export default {
   -webkit-text-fill-color: transparent;
   letter-spacing: -4px;
 } */
+
+.header.vcomp > .topheader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 60px;
+  background: white;
+  box-shadow: 0 0 10px rgba(0,0,0,0.1);
+  opacity: 0;
+  transform: translateY(-70px);
+  transition: all 0.5s;
+  z-index: 3;
+}
+
+.header.vcomp > .topheader.on {
+  opacity: 1;
+  transform: none;
+}
+
+.header.vcomp > .topheader > .logo {
+  position: absolute;
+  top: 5px;
+  left: 20px;
+  /* width: 100px; */
+  height: 50px;
+  /* background: orange; */
+  line-height: 50px;
+  /* background: -webkit-linear-gradient(315deg,#42d392 25%,#647eff); */
+}
+
+.header.vcomp > .topheader > .logo > .title {
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 25px;
+  font-weight: bold;
+  letter-spacing: -4px;
+  color: white;
+  background: -webkit-linear-gradient(315deg,#42d392 25%,#647eff);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  padding-right: 5px;
+}
 </style>
